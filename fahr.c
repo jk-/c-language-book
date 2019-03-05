e798b6833e2603dd7cf3d9d8e6232739966187b8
bas <basilieus@gmail.com>
2019-03-05 08:54:14 -0800

    1.2: Update fahr to use floats to be more accurate

--- fahr.c
+++ fahr.c
@@ -4,7 +4,7 @@
     for fahr = 0, 20, ..., 300 */
 int main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;  /* lower limit of temp */
@@ -13,8 +13,9 @@ int main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        /* change to float type */
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
