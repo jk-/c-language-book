4c97c6d8ddebed73b2021aac08dcb11ce6ff9fa3
bas <basilieus@gmail.com>
2019-03-06 11:12:04 -0800

    6.2: Structures and Functions

---
+++ structure-factory.c
@@ -0,0 +1,14 @@
+struct point {
+    int x;
+    int y;
+};
+
+/* makepoint: make a point from x and y */
+struct point makepoint(int x, inty)
+{
+    struct point temmp;
+
+    temp.x = x;
+    temp.y = y;
+    return temp;
+}
